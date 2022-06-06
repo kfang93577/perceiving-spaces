324cfb764cc3751077f282c4e306fabdbb4fff6e
Alastair Shipman <alastair.shipman@gmail.com>
2022-06-06 09:28:04 -0400

    now with config file, including output and input filepaths

---
+++ pythonProject/config.py
@@ -0,0 +1,7 @@
+output_file_path= './pythonProject/Output_files'
+
+output_image_path=f"{output_file_path}/images"
+output_video_path=f"{output_file_path}/videos"
+
+input_file_path='./pythonProject/Input_files'
+
